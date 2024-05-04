6ca47752569cacc392ad76a20c69d96a3eebc40c
Guilherme Tinen <gfmi0911@hotmail.com>
2024-05-04 10:53:11 -0300

    lombok addtions and DTO constructor

--- src/main/java/com/example/quizbe/domain/quiz/Quiz.java
+++ src/main/java/com/example/quizbe/domain/quiz/Quiz.java
@@ -1,14 +1,23 @@
 package com.example.quizbe.domain.quiz;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Table(name = "quiz")
 @Entity(name = "quiz")
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "quiz")
 public class Quiz {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
     private String name;
+
+    public Quiz(RequestQuizDto requestQuizDto) {
+        this.name = requestQuizDto.name();
+    }
 }
