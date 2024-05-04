5307a08d8d1a4e934aa28ed4a548d8dd28b94844
Guilherme Tinen <gfmi0911@hotmail.com>
2024-05-03 21:45:43 -0300

    initial commit

---
+++ src/main/java/com/example/quizbe/QuizbeApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quizbe/QuizbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
